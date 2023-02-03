b1509eac1021955a1bdae32b3d3e1e6fe54e9666
cauecbb <contato.cauecbb@gmail.com>
2023-02-03 09:21:25 -0300

    Estrutura BookPage

--- lib/pages/book_page.dart
+++ lib/pages/book_page.dart
@@ -6,11 +6,69 @@ class BookPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        color: Colors.brown[50],
-        child: const Center(
-          child: Text("Detalhes do Livro"),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              backgroundColor: Colors.white.withOpacity(0),
+              elevation: 0.0,
+              expandedHeight: 450,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                background: Image.asset(
+                  "assets/livro.jpg",
+                  width: double.infinity,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+          ];
+        },
+        body: ListView(
+          children: const <Widget>[
+            Padding(
+              padding: EdgeInsets.only(
+                top: 10,
+                left: 10,
+                right: 10,
+              ),
+              child: Text(
+                "Clean Code",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "Autor: Robert C. Martin",
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "Detalhes",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "Even bad code can function. But if code isn’t clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn’t have to be that way.",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
