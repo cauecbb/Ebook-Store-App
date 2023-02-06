2987010110c8956abc7d4901b64a5c8f2fdd8e9f
cauecbb <contato.cauecbb@gmail.com>
2023-02-06 11:50:43 -0300

    Adiciona Botão de adição de itens do carrinho

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -53,6 +53,68 @@ Widget bookItem() {
             fit: BoxFit.fitWidth,
           ),
         ),
+        Padding(
+          padding: const EdgeInsets.only(
+            top: 30,
+            left: 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text(
+                "Título do produto",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              const Text(
+                "\$200",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.blue,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 30,
+                width: 120,
+                decoration: const BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(5),
+                  ),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      width: 40,
+                      alignment: Alignment.center,
+                      child: TextButton(
+                        child: const Text("+"),
+                        onPressed: () => {},
+                      ),
+                    ),
+                    Container(
+                      width: 40,
+                      alignment: Alignment.center,
+                      child: const Text("1"),
+                    ),
+                    Container(
+                      width: 40,
+                      alignment: Alignment.center,
+                      child: TextButton(
+                        child: const Text("-"),
+                        onPressed: () => {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
       ],
     ),
   );
